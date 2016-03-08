e0d26b5bba65a72586c36c087bd3ea51acd6eef2
liangweigang <liangweigang@outlook.com>
2016-03-09 00:03:55 +0800

    Update non-blocking-tcp-connect.c
    add close socket at connect failed.

--- non-blocking-tcp-connect.c
+++ non-blocking-tcp-connect.c
@@ -1,5 +1,5 @@
 //the 3 steps of tcp non-blocking connect :
-//1. connect (with ocket fd has been set non-blocking).
+//1. connect (with socket fd has been set non-blocking).
 //2. check fd writable
 //3. get connect result with  getsockopt
 
@@ -10,7 +10,7 @@
 
 #include <sys/socket.h>
 #include <fcntl.h> // fctl
-#include <arpa/inet.h> //ServerAddr,IPPROTO_TCP
+#include <arpa/inet.h> //struct sockaddr_in, IPPROTO_TCP
 #include <errno.h> // errno
 
 int SetSocketNoBlock(int nSocket)
@@ -40,21 +40,32 @@ int SetSocketRST(int nSocket)
     return nRetCode;
 }
 
+void CloseSocket(int nSocket)
+{
+    if (nSocket <= 0)
+        return;
+
+    if(SetSocketRST(nSocket) < 0)
+        perror("set RST");
+
+    close(nSocket);
+}
+
 int Connect(char szIP[], int nPort, int* nRetSocktFd)
 {
     int                 nResult   = false;
-    int                 nSocketFd = -1;
+    int                 nSocket   = -1;
     int                 nRetCode  = 0;
     struct sockaddr_in  ServerAddr;
 
-    nSocketFd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-    if (nSocketFd == -1)
+    nSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+    if (nSocket == -1)
     {
         perror("fcntl");
         return false;
     }
 
-    nRetCode =  SetSocketNoBlock(nSocketFd);
+    nRetCode =  SetSocketNoBlock(nSocket);
     if (!nRetCode)
     {
         perror("SetSocketNoBlock");
@@ -67,7 +78,7 @@ int Connect(char szIP[], int nPort, int* nRetSocktFd)
     memset(ServerAddr.sin_zero, 0, 8);
 
     errno = 0;
-    nRetCode = connect(nSocketFd, (struct sockaddr*)&ServerAddr, sizeof(ServerAddr));
+    nRetCode = connect(nSocket, (struct sockaddr*)&ServerAddr, sizeof(ServerAddr));
 
     if (nRetCode != -1 && errno != EINPROGRESS) //EINPROGRESS connecting.
     {
@@ -75,11 +86,10 @@ int Connect(char szIP[], int nPort, int* nRetSocktFd)
         return false;
     }
 
-    *nRetSocktFd = nSocketFd;
+    *nRetSocktFd = nSocket;
 
     return true;
 }
-
 
 int main(int argc, char const *argv[])
 {
@@ -95,6 +105,7 @@ int main(int argc, char const *argv[])
     nRetCode = Connect(szIP, nPort, &nSocket);
     if (!nRetCode)
     {
+        CloseSocket(nSocket);
         perror("Connect");
         exit(EXIT_FAILURE);
     }
@@ -108,6 +119,7 @@ int main(int argc, char const *argv[])
         if (nRetCode < 0)
         {
           perror("select");
+          CloseSocket(nSocket);
           exit(EXIT_FAILURE);
         }
         if (nRetCode >0) // nSocket writable
@@ -118,15 +130,12 @@ int main(int argc, char const *argv[])
     nRetCode   = getsockopt(nSocket, SOL_SOCKET, SO_ERROR, &nResult, &nResultLen);
     if (nRetCode< 0 || nResult != 0) {
         perror("getsockopt");
-        close(nSocket);
+        CloseSocket(nSocket);
         exit(EXIT_FAILURE);
     }
 
     printf("connect success.\n");
 
-    if(SetSocketRST(nSocket) < 0)
-        perror("set RST");
-
-    close(nSocket);
+    CloseSocket(nSocket);
     return 0;
 }
